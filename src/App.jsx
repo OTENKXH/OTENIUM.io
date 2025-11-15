ed8bb1f668dd46c41143d79dce8d36cd7aafe295
OTEN <hossein.k777@gmail.com>
2025-11-15 16:43:37 +0330

    deploy final cut

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header/header.jsx'
 import MobileHeader from './components/header/mobile-header.jsx'
